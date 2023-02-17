a0999cea140eda7e7bdb78437d054083b90b3f33
Perryvw <perry.wesel@gmail.com>
2023-02-17 14:33:52 +0100

    Slightly nicer css

--- src/docpage.tsx
+++ src/docpage.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 import { SIDEBAR_WIDTH } from "./styles";
 
 export function DocPage() {
-    return <div id="content"
+    return <div id="content" className="markdown-body"
         style={{
             width: `calc(100% - ${SIDEBAR_WIDTH}px)`
         }}
